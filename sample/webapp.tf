ac0d7f958d8d667320bd83b1f5ce506c09e9c73d
Martin Brandl <mjibrandl@gmail.com>
2021-05-04 10:44:58 +0200

    added install sample

--- sample/webapp.tf
+++ sample/webapp.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "webapp" {
   name     = "example.resource"
   location = "West Europe"
+  tags = {
+    "Environment" = "dev"
+    "Purpose"     = "Workshop"
+  }
 }
