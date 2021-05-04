42c041155bc1b236bad3c31695b97a9e927054a6
Martin Brandl <mjibrandl@gmail.com>
2021-05-04 10:39:46 +0200

    added webapp.tf

---
+++ sample/main.tf
@@ -0,0 +1,21 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.57.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=1.4.0"
+    }
+    required_version = "0.15.1"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
---
+++ sample/webapp.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "webapp" {
+  name     = "example.resource"
+  location = "West Europe"
+}
